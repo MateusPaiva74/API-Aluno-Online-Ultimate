5c9715f802a970d8084092da768032b6d87dda3b
Mateus Andre <mateusandre74@gmail.com>
2024-06-04 19:58:48 -0300

    Criado o StudentController e InstructorController

---
+++ API/src/main/java/br/com/alunoonline/API/Controller/InstructorController.java
@@ -0,0 +1,51 @@
+package br.com.alunoonline.API.Controller;
+
+import br.com.alunoonline.API.Model.Instructor;
+import br.com.alunoonline.API.Model.Student;
+import br.com.alunoonline.API.Service.InstructorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("Instructor")
+public class InstructorController {
+
+    @Autowired
+    InstructorService instructorService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void create (@RequestBody Instructor instructor){
+        InstructorService.create(instructor);
+    }
+
+    @GetMapping("/all")
+    @ResponseStatus(HttpStatus.OK)
+    public List<Instructor> findAll(){
+        return instructorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+
+    public Optional<Instructor> findById(@PathVariable Long id){
+        return instructorService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+
+    public void update (@RequestBody Instructor instructor, @PathVariable Long id){
+        instructorService.update(id, instructor);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+
+    public void deleteById(@PathVariable Long id){
+        instructorService.deleteById(id);
+    }
+}
