fd980327d4bf906d7fbd5c509c4dbd62088d0356
Mateus Andre <mateusandre74@gmail.com>
2024-06-04 12:27:58 -0300

    Criado os packages, Enums, Service,Controller,Repository,Model e Dtos

---
+++ API/src/main/java/br/com/alunoonline/API/AlunoOnlineFinalApplication.java
@@ -0,0 +1,13 @@
+package br.com.alunoonline.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunoOnlineFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunoOnlineFinalApplication.class, args);
+	}
+
+}
---
+++ API/src/test/java/br/com/alunoonline/API/AlunoOnlineFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alunoonline.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoOnlineFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
